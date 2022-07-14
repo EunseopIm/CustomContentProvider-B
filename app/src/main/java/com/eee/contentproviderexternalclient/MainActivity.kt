6f7c8014d5075ec9b1d4d53e880acdd61a5fc49c
eunseop.im <esleem@solter.co.kr>
2022-07-14 18:07:52 +0900

    -

--- app/src/main/java/com/eee/contentproviderexternalclient/MainActivity.kt
+++ app/src/main/java/com/eee/contentproviderexternalclient/MainActivity.kt
@@ -1,30 +1,41 @@
 package com.eee.contentproviderexternalclient
 
-import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private val REQ_PERMISSIONS_CODE = 1312
     private val PERMISSIONS_STORAGE = arrayOf(
-        Manifest.permission.READ_EXTERNAL_STORAGE,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE
+        "com.app.first.READ_DATABASE",
+        "com.app.first.WRITE_DATABASE"
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        requestPermissions(PERMISSIONS_STORAGE, REQ_PERMISSIONS_CODE)
+        //requestPermissions(PERMISSIONS_STORAGE, REQ_PERMISSIONS_CODE)
+
+        runProvider()
+    }
+
+    private fun runProvider() {
+
+        val contentResolverHelper = ContentResolverHelper(this)
+        contentResolverHelper.insertCompanyTMRecord("외부1", "외부1")
+        contentResolverHelper.allItems()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
+        Log.v(">>>", "ob-1")
         if (requestCode == REQ_PERMISSIONS_CODE) {
 
+            Log.v(">>>", "ob-2")
             var granted = true
             for (result in grantResults) {
                 if (result != PackageManager.PERMISSION_GRANTED) {
@@ -33,11 +44,10 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
+            Log.v(">>>", "ob-3 : $granted")
             if (granted) {
 
-                val contentResolverHelper = ContentResolverHelper(this)
-                contentResolverHelper.insertCompanyTMRecord("외부1", "외부1")
-                contentResolverHelper.allItems()
+                runProvider()
 
             } else {
 
