7240cf415c3248880d342cd99c2c2274ff7bb267
eunseop.im <esleem@solter.co.kr>
2022-07-14 14:49:18 +0900

    - project init

---
+++ app/src/main/java/com/eee/contentproviderexternalclient/CommonUtils.kt
@@ -0,0 +1,14 @@
+package com.eee.contentproviderexternalclient
+
+import android.util.Log
+
+class CommonUtils {
+
+    companion object {
+
+        fun log(message: String) {
+
+            Log.v(">>>", message)
+        }
+    }
+}
