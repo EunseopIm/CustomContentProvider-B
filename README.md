267d172e0fd1a984d45928338dffdcf4724901e0
eunseop.im <eunseop.im@samsung.com>
2022-08-16 11:30:17 +0900

    -

---
+++ README.md
@@ -0,0 +1,2 @@
+# CustomContentProvider-B
+ContentProvider 간 데이터 공유 B앱
--- app/src/main/java/com/study/providerb/ContentResolverHelper.kt
+++ app/src/main/java/com/study/providerb/ContentResolverHelper.kt
@@ -9,14 +9,6 @@ import android.util.Log
 
 class ContentResolverHelper(private var mContext: Context) {
 
-    companion object {
-
-        private const val TABLE_NAME = "item"
-        private const val AUTHORITY = "com.study.providera.MyContentProvider"
-        private const val URL = "content://$AUTHORITY/$TABLE_NAME"
-        val CONTENT_URI: Uri = Uri.parse(URL)
-    }
-
     private var contentResolver: ContentResolver = mContext.contentResolver
 
     /**
@@ -24,7 +16,7 @@ class ContentResolverHelper(private var mContext: Context) {
      */
     fun getAllItems() {
 
-        val cursor = contentResolver.query(CONTENT_URI, null, null, null, null)
+        val cursor = contentResolver.query(MyContract.CONTENT_URI, null, null, null, null)
 
         if (cursor != null && cursor.count > 0) {
             while (cursor.moveToNext()) {
@@ -47,7 +39,7 @@ class ContentResolverHelper(private var mContext: Context) {
      */
     fun getItem(id: Long) {
 
-        val cursor = contentResolver.query(CONTENT_URI, null, "id", arrayOf("$id"), null)
+        val cursor = contentResolver.query(MyContract.CONTENT_URI, null, "id", arrayOf("$id"), null)
 
         if (cursor != null && cursor.count > 0) {
 
@@ -72,7 +64,7 @@ class ContentResolverHelper(private var mContext: Context) {
     fun insertItem(title: String, content: String) {
 
         val contentValues = generateItem(title, content)
-        contentResolver.insert(CONTENT_URI, contentValues)
+        contentResolver.insert(MyContract.CONTENT_URI, contentValues)
     }
 
     /**
@@ -80,9 +72,9 @@ class ContentResolverHelper(private var mContext: Context) {
      */
     fun removeItem(id: Long) {
 
-        val url = "$URL/$id"
+        val uriInfo = "${MyContract.URI_STRING}/$id"
 
-        contentResolver.delete(Uri.parse(url), "id", arrayOf("$id"))
+        contentResolver.delete(Uri.parse(uriInfo), "id", arrayOf("$id"))
     }
 
     /**
@@ -104,7 +96,7 @@ class ContentResolverHelper(private var mContext: Context) {
 
         var value: String? = null
 
-        val bundle: Bundle? = contentResolver.call(CONTENT_URI, "getId", null, null)
+        val bundle: Bundle? = contentResolver.call(MyContract.CONTENT_URI, "getId", null, null)
 
         bundle?.let {
 
